9889361b5d8ad3de9503f67a15f65e28aca5acd4
Lutz Fischer <lfischer@staffmail.ed.ac.uk>
2018-07-03 14:46:57 +0200

    Denoise filter per default dose not remove precursor peaks and can also be instructed to not remove defined m/z values as well (e.g. reporter ions). Isotope clusters belonging to these peaks will also be kept

--- src/rappsilber/ms/dataAccess/filter/spectrafilter/Denoise.java
+++ src/rappsilber/ms/dataAccess/filter/spectrafilter/Denoise.java
@@ -15,22 +15,59 @@
  */
 package rappsilber.ms.dataAccess.filter.spectrafilter;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import rappsilber.config.AbstractRunConfig;
 import rappsilber.ms.dataAccess.AbstractStackedSpectraAccess;
 import rappsilber.ms.spectra.Spectra;
+import rappsilber.utils.Util;
 
 /**
  *
  * @author Lutz Fischer <l.fischer@ed.ac.uk>
  */
 public class Denoise  extends AbstractStackedSpectraAccess {
-    double minMZ=Double.MIN_VALUE;
-    double maxMZ=Double.MAX_VALUE;
-    double window=100d;
-    int peaks=20;
+
+
+    private double minMZ=Double.MIN_VALUE;
+    private double maxMZ=Double.MAX_VALUE;
+    private double window=100d;
+    private int peaks=20;
+    private boolean keepPrecursor=true;
+    private HashSet<Double> keepPeaks=new HashSet<>();
+    
 
     public Denoise() {
     }
 
+    
+    public Denoise(String settings) {
+        String[] set = settings.split(";");
+        for (String s: set) {
+            String[] args = s.split(":");
+            String a = args[0].toLowerCase().trim();
+            String v = args[1].toLowerCase().trim();
+            if (a.contentEquals("minmz")) {
+                minMZ = Double.parseDouble(v);
+            } else if (a.contentEquals("maxmz")) {
+                maxMZ = Double.parseDouble(v);
+            } else if (a.contentEquals("peaks")) {
+                peaks = (int)Double.parseDouble(v);
+            } else if (a.contentEquals("window")) {
+                window = Double.parseDouble(v);
+            } else if (a.contentEquals("keepprecursor")) {
+                keepPrecursor = AbstractRunConfig.getBoolean(v, keepPrecursor);
+            } else if (a.contentEquals("keeppeaks")) {
+                String[] peaks = v.split(";");
+                for (String p : peaks) {
+                    Double d=new Double(p.trim());
+                    keepPeaks.add(d);
+                }
+            }
+            
+        }
+    }
     
     public Denoise(double minMZ,double maxMZ, double window,int peaks) {
         this(window, peaks);
@@ -45,12 +82,126 @@ public class Denoise  extends AbstractStackedSpectraAccess {
     
     
     public Spectra next() {
-        if (minMZ > Double.MIN_VALUE || maxMZ<Double.MAX_VALUE ) {
-            return m_InnerAcces.next().cloneTopPeaksRolling(peaks, window, minMZ, maxMZ);
+        Spectra n =m_InnerAcces.next();
+
+        // we try to keep peaks that could be related to the precursor
+        double p_mz= n.getPrecurserMZ();
+        HashSet<Double> mz = new HashSet<>(7*7);
+        
+        if (keepPrecursor) {
+            // so if we assume that there is a possibility for error 
+            // in the MS1 precursor detection then we should keep all peaks that 
+            // could belong to the precursor in any charge state and with any number 
+            // of missing isotopic peaks
+            for (int c=1;c<=7;c++) {
+                for (int p =-3;p<0;p++) {
+                    mz.add(p_mz+Util.C13_MASS_DIFFERENCE*p/c);
+                }
+                for (int p =1;p<4;p++) {
+                    mz.add(p_mz+Util.C13_MASS_DIFFERENCE*p/c);
+                }
+            }
         }
-        return m_InnerAcces.next().cloneTopPeaksRolling(peaks, window);
+        
+        if (minMZ > Double.MIN_VALUE || maxMZ<Double.MAX_VALUE ) {
+            return n.cloneTopPeaksRolling(peaks, window, minMZ, maxMZ,mz);
+        }
+        return n.cloneTopPeaksRolling(peaks, window,mz);
     }
 
+    
+    /**
+     * @return the minMZ
+     */
+    public double getMinMZ() {
+        return minMZ;
+    }
+
+    /**
+     * @param minMZ the minMZ to set
+     */
+    public void setMinMZ(double minMZ) {
+        this.minMZ = minMZ;
+    }
+
+    /**
+     * @return the maxMZ
+     */
+    public double getMaxMZ() {
+        return maxMZ;
+    }
+
+    /**
+     * @param maxMZ the maxMZ to set
+     */
+    public void setMaxMZ(double maxMZ) {
+        this.maxMZ = maxMZ;
+    }
+
+    /**
+     * @return the window
+     */
+    public double getWindow() {
+        return window;
+    }
+
+    /**
+     * @param window the window to set
+     */
+    public void setWindow(double window) {
+        this.window = window;
+    }
+
+    /**
+     * @return the peaks
+     */
+    public int getPeaks() {
+        return peaks;
+    }
+
+    /**
+     * @param peaks the peaks to set
+     */
+    public void setPeaks(int peaks) {
+        this.peaks = peaks;
+    }
+    
  
+    /**
+     * @return the keepPrecursor
+     */
+    public boolean KeepPrecursorPeaks() {
+        return keepPrecursor;
+    }
+
+    /**
+     * @param keepPrecursor the keepPrecursor to set
+     */
+    public void setKeepPrecursorPeaks(boolean keepPrecursor) {
+        this.keepPrecursor = keepPrecursor;
+    }
+
+    /**
+     * @return the keepPeaks
+     */
+    public HashSet<Double> getKeepPeaks() {
+        return keepPeaks;
+    }
+
+    /**
+     * @param keepPeaks the keepPeaks to set
+     */
+    public void setKeepPeaks(Collection<Double> keepPeaks) {
+        this.keepPeaks = new HashSet<>(keepPeaks);
+    }
+
+    /**
+     * @param keepPeak refister an m/z value that should not be filtered out by 
+     * the noise filter
+     */
+    public void addKeepPeaks(Double keepPeak) {
+        this.keepPeaks.add(keepPeak);
+    }
+
     
 }
