5703c440ae35eb9a2ab55b7b1208183f34e8a8fc
Lutz Fischer <lfischer@staffmail.ed.ac.uk>
2020-05-04 15:54:13 +0200

    Version 1.7.3.2

--- src/main/java/rappsilber/utils/XiVersion.java
+++ src/main/java/rappsilber/utils/XiVersion.java
@@ -21,10 +21,10 @@ package rappsilber.utils;
  * @author Lutz Fischer <l.fischer@ed.ac.uk>
  */
 public class XiVersion {
-    public static Version version = new Version(1, 7, 3, 1);
+    public static Version version = new Version(1, 7, 3, 2);
      
     
-    public static final String changes = "Version V1.7.3.1\n" +
+    public static final String changes = "Version V1.7.3.2\n" +
                                 "  * Backported bugfix from 1.7.5.\n" +
                                 "Version V1.7.3\n" +
                                 "  * Improved Loading of configs.\n" +
